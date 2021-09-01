c5ec254ec17d9d8ee1f810eee3a089e5dfbeaac6
Marina-sky <75041628+Marina-sky@users.noreply.github.com>
2021-09-01 22:22:01 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
