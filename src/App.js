5847f0810d2c3fc178cf1dec55b3ec722da2800e
Marina-sky <75041628+Marina-sky@users.noreply.github.com>
2021-09-02 21:56:50 +0200

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Marina Momčilov</small>
+        </footer>
+      </div>
     </div>
   );
 }
