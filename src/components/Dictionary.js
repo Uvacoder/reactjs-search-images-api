166c820865eee93f93afc7918bf714abe1776377
Marina-sky <75041628+Marina-sky@users.noreply.github.com>
2021-09-10 21:33:27 +0200

    Added section for displaying photos

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import "../layouts/Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f9170000100000193d095e4d8f3459580dfd655b311f608";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -27,24 +40,29 @@ export default function Dictionary(props) {
   }
 
   function load() {
-      setLoaded(true);
-      search();
+    setLoaded(true);
+    search();
   }
 
   if (loaded) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word do you want to look up?</h1>
+          <h2>What word do you want to look up?</h2>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
-      load();
-      return null;
+    load();
+    return null;
   }
 }
